1cc71708f7e828a4947d88109a85abe4df79ed52
Andrés <andres.valle@decaduo.com>
2026-07-11 20:25:40 -0300

    install lazy vim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,3 @@
---Enables to show the line number
-vim.opt.number = true
---Enables to highlight the current line
-vim.opt.cursorline = true
---Disables the relative line number
-vim.opt.relativenumber = false
---Sets the with of each indentation
-vim.opt.shiftwidth = 4
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here

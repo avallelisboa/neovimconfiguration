94dc9e99d133f64c230a2d677b0472ea49acd984
Andrés <andres.valle@decaduo.com>
2026-07-12 17:26:58 -0300

    Fix issue with configuration of treesitter

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -3,24 +3,28 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = {
-		enable = true,
-	    }
-	    indent = { enable = true},
-	    ensure_installed = {
-		"c",
-		"cpp",
-		"rust",
-		"lua",
-		"tsx",
-		"typescript",
-		"php",
-		"python",
-		"javascript"
-	    },
-	    auto_install = true,
-	})
+        -- Automatically turn on treesitter highlighting and indents when entering a file
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+
+        -- Handle your language parser installations manually
+        local ensure_installed = {
+            "c", "cpp", "rust", "lua", "tsx", 
+            "typescript", "php", "python", "javascript"
+        }
+        
+        -- Pull down any missing language parsers
+        local installed = require('nvim-treesitter.config').get_installed()
+        local to_install = vim.iter(ensure_installed):filter(function(p)
+            return not vim.tbl_contains(installed, p)
+        end):totable()
+
+        if #to_install > 0 then
+            require('nvim-treesitter').install(to_install)
+        end
     end
 }
